45448e11e145aa5cd2bf216715691bfb87212c78
juanmarsilva <juanmar_silva@hotmail.com>
2022-07-15 23:47:19 -0300

    adelante poquito del back

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,22 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const axios = require('axios');
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+
+// const getAllPokemons = async () => {
+//     axios.get('https://pokeapi.co/api/v2/pokemon')
+// }
+
+
+
+// router.get('/pokemons', async (req, res) => {
+    
+// })
 
 
 module.exports = router;
