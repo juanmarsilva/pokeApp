715773e8ef4a5de9ff430a64c1fb920698a46c6f
juanmarsilva <juanmar_silva@hotmail.com>
2022-07-15 23:43:51 -0300

    Base de datos terminada por ahora

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -4,9 +4,39 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    health: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
+    },
+    attack: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
+    },
+    defense: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
+    },
+    speed: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
+    },
+    height: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
+    },
+    weight: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
+    }
   });
 };
